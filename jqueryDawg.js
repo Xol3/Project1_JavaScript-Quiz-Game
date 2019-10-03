44163995f054779e2cb7b9622e32a10ad71227d4
Francisco Rojas <24814380+Xol3@users.noreply.github.com>
2019-10-03 12:59:06 -0500

    Update jqueryDawg.js
    fixed format and some logic

--- jqueryDawg.js
+++ jqueryDawg.js
@@ -1,50 +1,49 @@
-  	let questions = ['What is the DOM?','What is Javascript?','What is a method?', 'What is an arrow function'];
-    let answerSet = [['questionset1','wow','hero','hike'],['questionset2','show','answer3','answer4'], ['questionset3','death','answer3','answer4'],['questionset4','silence','answer3','answer4']];
-   
-    let correctAnswers = ['answer1','answer2','answer3','answer4'];
-  	let arrayOfAnswer = [];
-    let score = 0;
-    let i = 0;
-    let render = function (template, node) {
-      node.innerHTML = template;
-    };
-    //rendering question 1
-  	render(questions[0],document.querySelector('#mainquestion'));
-    
-    for(let k = 0; k < answerSet[0].length; k++) {
-    render(answerSet[0][k],document.querySelector('#one' + k));
-    }
-    
-      //button onClick
-  	  $('#btn').on('click', function() {
-            
+let questions = ['What is the DOM?', 
+'What is js?', 
+'What is a method?', 
+'What is an arrow function'];
+let answerSet = [['DocumentObjectModel', '1', '1', '1'], 
+['Javascript', '2', '2', '2'], 
+['a object function', '3', '3', '3'], 
+['4', '4', '4', 'es6 syntax for function']];
 
-            answer =  
-              $('input[name=question]:checked', 
-                '#questionaire').val(); 
-            arrayOfAnswer.push(answer); 
-         render(questions[i],document.querySelector('#mainquestion'));
+let correctAnswers = ["answer1","answer1","answer1","answer4" ];
+let arrayOfAnswer = [];
+let score = 0;
+let i = 0;
+let render = function (template, node) {
+  node.innerHTML = template;
+};
+//rendering question 1
+render(questions[0], document.querySelector('#mainquestion'));
+//rendering answers
+for (let k = 0; k < answerSet[0].length; k++) {
+  render(answerSet[0][k], document.querySelector('#answ' + k));
+}
 
-      for(let j = 0; j < answerSet[i].length; j++) {
-        render(answerSet[i][j],document.querySelector('#one' + j));
+//button onClick
+$('#btn').on('click', function () {
+  //push the selected answer to the array
+  answer = $('input[name=question]:checked','#questionaire').val(); 
+  arrayOfAnswer.push(answer);
+
+  //check if you reach the end render score
+  if (i === 3) {
+    for (let x = 0; x < 4; x++) {
+      if (correctAnswers[x] === arrayOfAnswer[x]) {
+        score++
       }
-    
-            console.log(arrayOfAnswer);
-   
-              i++;
-              if(arrayOfAnswer.length === 4) {
-                for(let x= 0; x< 4; x++ ){
-                  if(correctAnswers[x]===arrayOfAnswer[x]){
-                    score++  
-                  }
-                }
+    }
+    //changed the alert to cahange the questionaire node to score string
+    document.getElementById("questionaire").innerHTML = 'Your Score is ' + score;
+    return;
+  }else{
+    //else render the next answer/quetion also remember to raise the counter
+    i++;
+    render(questions[i], document.querySelector('#mainquestion'));
+    for (let j = 0; j < answerSet[i].length; j++) {
+      render(answerSet[i][j], document.querySelector('#answ' + j));
+    }
+  }
+})
 
-              alert('nice bro you are a genious you got ' +score)
-              return;
-              } 
-        })
-       
-	   // if(answer) {
-	   	
-	   // }
-  	
